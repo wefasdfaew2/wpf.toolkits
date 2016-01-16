90e576aa607056793ddc080a15990ae5e978f224
tienquang <oclockvn@gmail.com>
2016-01-16 15:19:12 +0700

    added bindable base class

---
+++ WTFToolkits.BindableBase/BindableBase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace WTFToolkits.BindableBase
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///     Multicast event for property change notifications.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        /// <summary>
+        ///     Checks if a property already matches a desired value.  Sets the property and
+        ///     notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to a property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <param name="propertyName">
+        ///     DisplayName of the property used to notify listeners.  This
+        ///     value is optional and can be provided automatically when invoked from compilers that
+        ///     support CallerMemberName.
+        /// </param>
+        /// <returns>
+        ///     True if the value was changed, false if the existing value matched the
+        ///     desired value.
+        /// </returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        ///     Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">
+        ///     DisplayName of the property used to notify listeners.  This
+        ///     value is optional and can be provided automatically when invoked from compilers
+        ///     that support <see cref="CallerMemberNameAttribute" />.
+        /// </param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class ValidatableBase : NotifyPropertyChangedBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged = delegate { };
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (_errors.ContainsKey(propertyName))
+                return _errors[propertyName];
+            else
+                return null;
+        }
+
+        public bool HasErrors
+        {
+            get { return _errors.Count > 0; }
+        }
+
+        protected override void SetProperty<T>(ref T member, T val, [CallerMemberName] string propertyName = null)
+        {
+            base.SetProperty<T>(ref member, val, propertyName);
+            ValidateProperty(propertyName, val);
+        }
+
+        private void ValidateProperty<T>(string propertyName, T value)
+        {
+            var results = new List<ValidationResult>();
+            ValidationContext context = new ValidationContext(this);
+            context.MemberName = propertyName;
+            Validator.TryValidateProperty(value, context, results);
+
+            if (results.Any())
+            {
+
+                _errors[propertyName] = results.Select(c => c.ErrorMessage).ToList();
+            }
+            else
+            {
+                _errors.Remove(propertyName);
+            }
+            ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ WTFToolkits/SimpleModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WTFToolkits.BindableBase;
+
+namespace WTFToolkits
+{
+    public class SimpleModel : ValidatableBase
+    {
+        public int Id { get; set; }
+
+
+        private string _name;
+        [Required(ErrorMessage = "Model name is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+
+        private string _description;
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(50, MinimumLength = 3)]        
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+    }
+}
